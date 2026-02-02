04e0a196242a771d294657292c2c5e7ee1f3ae9d
duy <nmnduy@gmail.com>
2026-02-02 12:09:15 +0700

    chore: bump version to 0.24.3

--- src/version.h
+++ src/version.h
@@ -9,20 +9,20 @@
 #define VERSION_H
 
 // Version string (e.g., "0.0.2", "1.0.0", "1.2.3-beta.1")
-#define KLAWED_VERSION "0.24.2"
+#define KLAWED_VERSION "0.24.3"
 
 // Version components for programmatic use
 #define KLAWED_VERSION_MAJOR 0
 #define KLAWED_VERSION_MINOR 24
-#define KLAWED_VERSION_PATCH 2
+#define KLAWED_VERSION_PATCH 3
 
 // Version as numeric value for comparisons (e.g., 0x000002)
-#define KLAWED_VERSION_NUMBER 0x001802
+#define KLAWED_VERSION_NUMBER 0x001803
 
 // Build timestamp (automatically generated)
 #define KLAWED_BUILD_TIMESTAMP "2026-02-02"
 
 // Full version string with build info
-#define KLAWED_VERSION_FULL "0.24.2 (built 2026-02-02)"
+#define KLAWED_VERSION_FULL "0.24.3 (built 2026-02-02)"
 
 #endif // VERSION_H
