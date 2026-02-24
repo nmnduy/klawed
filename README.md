4c744db7539fc0d24fe1d7ff874534e588d69692
duy <nmnduy@gmail.com>
2026-02-25 00:17:44 +0700

    chore: bump version to 0.29.31

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ sudo apt-get install libcurl4-openssl-dev libcjson-dev portaudio19-dev build-ess
 
 **Recommended: Use stable release**
 ```bash
-git clone --branch v0.29.30 https://github.com/nmnduy/klawed.git
+git clone --branch v0.29.31 https://github.com/nmnduy/klawed.git
 cd klawed
 make
 ```
--- src/version.h
+++ src/version.h
@@ -9,20 +9,20 @@
 #define VERSION_H
 
 // Version string (e.g., "0.0.2", "1.0.0", "1.2.3-beta.1")
-#define KLAWED_VERSION "0.29.30"
+#define KLAWED_VERSION "0.29.31"
 
 // Version components for programmatic use
 #define KLAWED_VERSION_MAJOR 0
 #define KLAWED_VERSION_MINOR 29
-#define KLAWED_VERSION_PATCH 30
+#define KLAWED_VERSION_PATCH 31
 
 // Version as numeric value for comparisons (e.g., 0x000002)
-#define KLAWED_VERSION_NUMBER 0x001d1e
+#define KLAWED_VERSION_NUMBER 0x001d1f
 
 // Build timestamp (automatically generated)
 #define KLAWED_BUILD_TIMESTAMP "2026-02-25"
 
 // Full version string with build info
-#define KLAWED_VERSION_FULL "0.29.30 (built 2026-02-25)"
+#define KLAWED_VERSION_FULL "0.29.31 (built 2026-02-25)"
 
 #endif // VERSION_H
