45b0cee50c7cfdf4f05c79b1ecda45116a155966
duy <nmnduy@gmail.com>
2026-04-08 13:59:22 +0700

    chore: bump version to 0.32.38

--- src/version.h
+++ src/version.h
@@ -9,20 +9,20 @@
 #define VERSION_H
 
 // Version string (e.g., "0.0.2", "1.0.0", "1.2.3-beta.1")
-#define KLAWED_VERSION "0.32.37"
+#define KLAWED_VERSION "0.32.38"
 
 // Version components for programmatic use
 #define KLAWED_VERSION_MAJOR 0
 #define KLAWED_VERSION_MINOR 32
-#define KLAWED_VERSION_PATCH 37
+#define KLAWED_VERSION_PATCH 38
 
 // Version as numeric value for comparisons (e.g., 0x000002)
-#define KLAWED_VERSION_NUMBER 0x002025
+#define KLAWED_VERSION_NUMBER 0x002026
 
 // Build timestamp (automatically generated)
 #define KLAWED_BUILD_TIMESTAMP "2026-04-08"
 
 // Full version string with build info
-#define KLAWED_VERSION_FULL "0.32.37 (built 2026-04-08)"
+#define KLAWED_VERSION_FULL "0.32.38 (built 2026-04-08)"
 
 #endif // VERSION_H
