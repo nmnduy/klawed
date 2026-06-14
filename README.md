ee78a81e674bb03583e2d739ffc342f11be7f855
duy <nmnduy@gmail.com>
2026-06-15 00:23:29 +0700

    chore: bump version to 0.34.0

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ sudo apt-get install libcurl4-openssl-dev libcjson-dev libbsd-dev libncurses-dev
 ### Build
 
 ```bash
-git clone --branch v0.33.0 https://github.com/nmnduy/klawed.git
+git clone --branch v0.34.0 https://github.com/nmnduy/klawed.git
 cd klawed
 make
 ```
--- src/version.h
+++ src/version.h
@@ -9,20 +9,20 @@
 #define VERSION_H
 
 // Version string (e.g., "0.0.2", "1.0.0", "1.2.3-beta.1")
-#define KLAWED_VERSION "0.33.0"
+#define KLAWED_VERSION "0.34.0"
 
 // Version components for programmatic use
 #define KLAWED_VERSION_MAJOR 0
-#define KLAWED_VERSION_MINOR 33
+#define KLAWED_VERSION_MINOR 34
 #define KLAWED_VERSION_PATCH 0
 
 // Version as numeric value for comparisons (e.g., 0x000002)
-#define KLAWED_VERSION_NUMBER 0x002100
+#define KLAWED_VERSION_NUMBER 0x002200
 
 // Build timestamp (automatically generated)
-#define KLAWED_BUILD_TIMESTAMP "2026-06-13"
+#define KLAWED_BUILD_TIMESTAMP "2026-06-15"
 
 // Full version string with build info
-#define KLAWED_VERSION_FULL "0.33.0 (built 2026-06-13)"
+#define KLAWED_VERSION_FULL "0.34.0 (built 2026-06-15)"
 
 #endif // VERSION_H
