d7e2b8f9d1c464684d1ebaa77178318709d23787
registry-ci <ci@registry.kasafox.com>
2026-07-30 23:28:49 +0000

    chore: bump version to 0.36.26

--- src/version.h
+++ src/version.h
@@ -9,20 +9,20 @@
 #define VERSION_H
 
 // Version string (e.g., "0.0.2", "1.0.0", "1.2.3-beta.1")
-#define KLAWED_VERSION "0.36.25"
+#define KLAWED_VERSION "0.36.26"
 
 // Version components for programmatic use
 #define KLAWED_VERSION_MAJOR 0
 #define KLAWED_VERSION_MINOR 36
-#define KLAWED_VERSION_PATCH 25
+#define KLAWED_VERSION_PATCH 26
 
 // Version as numeric value for comparisons (e.g., 0x000002)
-#define KLAWED_VERSION_NUMBER 0x002419
+#define KLAWED_VERSION_NUMBER 0x00241a
 
 // Build timestamp (automatically generated)
 #define KLAWED_BUILD_TIMESTAMP "2026-07-30"
 
 // Full version string with build info
-#define KLAWED_VERSION_FULL "0.36.25 (built 2026-07-30)"
+#define KLAWED_VERSION_FULL "0.36.26 (built 2026-07-30)"
 
 #endif // VERSION_H
